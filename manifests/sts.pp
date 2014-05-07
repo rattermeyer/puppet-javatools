6b1c26c12a98a2c76a0037aba3e1336e54a08bc3
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-05-07 22:29:50 +0200

    destination changed

--- manifests/sts.pp
+++ manifests/sts.pp
@@ -1,10 +1,11 @@
 class javatools::sts {
 	archive { 'sts-3.4.0':
-		ensure 	=> present,
-		url 	=> 'http://download.springsource.com/release/STS/3.4.0/dist/e4.3/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
-		target 	=> '/opt',
-		timeout => 600,
-		checksum => false
+		ensure 		 => present,
+		url 		 => 'http://download.springsource.com/release/STS/3.4.0/dist/e4.3/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
+		target 		 => '/opt/springsource',
+		strip_components => 1,
+		timeout 	 => 600,
+		checksum 	 => false
 	}
 	file { '/home/dev/Desktop/sts.desktop' :
                 ensure  => present,
