faf7de0506f1a4b44fa510c48953298d972a0d33
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-05-07 20:28:22 +0200

    updating extraction path

--- manifests/sts.pp
+++ manifests/sts.pp
@@ -2,7 +2,7 @@ class javatools::sts {
 	archive { 'sts-3.4.0':
 		ensure 	=> present,
 		url 	=> 'http://download.springsource.com/release/STS/3.4.0/dist/e4.3/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
-		target 	=> '/opt/springsource',
+		target 	=> '/opt',
 		timeout => 600,
 		checksum => false
 	}
