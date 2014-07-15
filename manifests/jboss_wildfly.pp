ed499cb1d918e61a604883a11e8800c167efbe7a
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-07-15 18:28:26 +0000

    version update

--- manifests/jboss_wildfly.pp
+++ manifests/jboss_wildfly.pp
@@ -1,15 +1,14 @@
 class javatools::jboss_wildfly {
-	archive { 'jboss-wildfly-8.0.0':
+	archive { 'jboss-wildfly-8.1.0':
 		ensure 	=> present,
-		url 	=> 'http://download.jboss.org/wildfly/8.0.0.Final/wildfly-8.0.0.Final.tar.gz',
+		url 	=> 'http://download.jboss.org/wildfly/8.1.0.Final/wildfly-8.1.0.Final.tar.gz',
 		target 	=> '/opt/jboss-wildfly',
 		timeout => 600,
 		checksum => false
-	}
-
+	}->
 	file { '/opt/jboss-wildfly/current':
 		ensure => 'link',
-		target => '/opt/jboss-wildfly/wildfly-8.0.0.Final',
-		require => Archive['jboss-wildfly-8.0.0']
+		target => '/opt/jboss-wildfly/wildfly-8.1.0.Final',
+		require => Archive['jboss-wildfly-8.1.0']
 	}
 }
--- manifests/sts.pp
+++ manifests/sts.pp
@@ -1,12 +1,12 @@
 class javatools::sts {
 	archive { 'sts-3.4.0':
 		ensure 		 => present,
-		url 		 => 'http://download.springsource.com/release/STS/3.4.0/dist/e4.3/spring-tool-suite-3.4.0.RELEASE-e4.3.1-linux-gtk-x86_64.tar.gz',
+		url 		 => 'http://download.springsource.com/release/STS/3.6.0/dist/e4.4/spring-tool-suite-3.6.0.RELEASE-e4.4-linux-gtk-x86_64.tar.gz',
 		target 		 => '/opt/springsource',
 		strip_components => 1,
 		timeout 	 => 600,
 		checksum 	 => false
-	}
+	}->
 	file { '/home/dev/Desktop/sts.desktop' :
                 ensure  => present,
                 source  => 'puppet:///modules/javatools/sts.desktop',
